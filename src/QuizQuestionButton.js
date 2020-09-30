f7b9a35616bda492a84d3de99fb604e58dc7fceb
gpurvi001 <gale_purvis@cable.comcast.com>
2020-09-30 12:43:06 -0600

    added QuizQuestionButton.js component

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -3,15 +3,9 @@ import React, { Component } from 'react'
 
 class QuizQuestionButton extends Component {
 
-    constructor(props){
-        super(props);
-        this.state = {quiz_position: 1 }
-    }
     render() {
         return (
-            <li>
-            <button type="button">Click Me!</button>
-            </li>
+            <li><button>{this.props.button_text}</button></li>
         )
     }
 
