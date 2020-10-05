2e6e6d523c3a2f18b0559d7729e1b0c2da2c2e62
gpurvi001 <gale_purvis@cable.comcast.com>
2020-10-05 10:04:55 -0600

    add logic

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
-
+  handleClick(buttonText) {
+    if(buttonText === this.props.quiz_question.answer) {
+      this.props.showNextQuestionHandler()
+    }
+  }
   render() {
     return (
       <main>
@@ -12,8 +16,9 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            {this.props.questions.map((question, index) => (<QuizQuestionButton button_text={this.props.quiz_question.answer_options[question]}/>))}
-            console.log(props)
+            {this.props.quiz_question.answer_options.map((answer_option, index) => {
+              return <QuizQuestionButton key={index} button_text={answer_option} clickHandler={this.handleClick.bind(this)} />
+            })}
           </ul>
         </section>
       </main>      
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 
-
 class QuizQuestionButton extends Component {
-
-    render() {
-        return (
-            <li><button>{this.props.button_text}</button></li>
-        )
-    }
-
+  handleClick() {
+    this.props.clickHandler(this.props.button_text)
+    //this will call the handler click method in the quiz question 
+    //component when a button is pressed and pass that button_text value to that method
+  }
+  render() {
+    return (
+      <li>
+        <button onClick={this.handleClick.bind(this)}>{this.props.button_text}</button>
+      </li>
+    )
+  }
 }
 
 export default QuizQuestionButton
